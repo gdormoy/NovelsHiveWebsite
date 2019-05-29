9de88bef8c57ec13a0677e4b417940e4e9f92a88
Unknown <guenin.vincent4@gmail.com>
2019-05-29 15:23:19 +0200

    [UPD] config templating

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,7 @@
-'use strict'
+'use strict';
+
 module.exports = {
   NODE_ENV: '"production"',
   API_LOCATION: '"http://localhost:3000/api"',
   SERVER_DOES_NOT_RESPOND_ERROR_MESSAGE: '\'Whooops the server is broken. We are fixing it, try again later :)\''
-}
+};
