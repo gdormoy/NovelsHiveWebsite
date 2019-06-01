f7b47ef19c4c34a1525d7797c8e3a8ae58841ab7
Vincent Guénin <guenin.vincent4@gmail.com>
2019-06-01 15:37:02 +0200

    [ADD] my-stories page

---
+++ src/components/MyStories.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="myStories">
+    <h1>My stories</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyStories'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import auth from '../middleware/auth'
 import HelloWorld from '@/components/HelloWorld'
 import Register from '@/components/Register'
 import Login from '@/components/Login'
-import auth from '../middleware/auth'
+import MyStories from '../components/MyStories'
 
 Vue.use(Router)
 
@@ -26,6 +27,11 @@ const router = new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/my-stories',
+      name: 'my-stories',
+      component: MyStories
     }
   ]
 })
