0c0293b9240084568d89a7128ba2fc2371076cb2
Vincent Guénin <guenin.vincent4@gmail.com>
2019-06-02 23:00:35 +0200

    [ADD] Display of completed chapters

--- src/components/MyStories.vue
+++ src/components/MyStories.vue
@@ -1,53 +1,30 @@
-<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+<template>
   <div id="myStories" data-app>
     <h1>My stories</h1>
 
     <h2>Drafts</h2>
-
-    <v-container fluid grid-list-md>
-      <v-data-iterator
-        :items="draftChapters"
-        :rows-per-page-items="rowsPerPageItems"
-        :pagination.sync="pagination"
-        content-tag="v-layout"
-        no-results-text="You don't have any draft yet."
-        row
-        wrap
-      >
-        <template v-slot:item="props">
-          <v-flex
-            xs12
-            sm6
-            md4
-            lg3
-          >
-            <chapter-icon :chapter="props.item.chapter"></chapter-icon>
-          </v-flex>
-        </template>
-      </v-data-iterator>
-    </v-container>
+    <chapter-icon-list :chapters="draftChapters"></chapter-icon-list>
 
     <h2>Completed</h2>
+    <div v-for="story in completedChapters" :key="story.storyName">
+      <chapter-icon-list :chapters="story.chapters" :story-name="story.storyName"></chapter-icon-list>
+    </div>
 
   </div>
 </template>
 
 <script>
-import ChapterIcon from './ChapterIcon'
+import ChapterIconList from './ChapterIconList'
 
 export default {
   name: 'MyStories',
-  components: {ChapterIcon},
+  components: {ChapterIconList},
   data () {
     return {
       stories: [],
       draftChapters: [],
       completedChapters: [],
-      tempCompletedChapters: [],
-      rowsPerPageItems: [4],
-      pagination: {
-        rowsPerPage: 4
-      }
+      tempCompletedChapters: []
     }
   },
   mounted () {
@@ -68,7 +45,7 @@ export default {
             if (chapter.online) {
               this.tempCompletedChapters.chapters.push(chapter)
             } else {
-              this.draftChapters.push({chapter})
+              this.draftChapters.push(chapter)
             }
           }, this)
 
@@ -79,7 +56,6 @@ export default {
       })
       .finally(() => {
         this.$store.state.loader = false
-        console.log(this.$data.completedChapters)
       })
   }
 }
