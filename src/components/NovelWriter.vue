8441a7be8b1f57ef35271a216aa21ebef5fac871
Vincent Guénin <guenin.vincent4@gmail.com>
2019-06-27 18:01:34 +0200

    [ADD] story picker on chapter writer

--- src/components/NovelWriter.vue
+++ src/components/NovelWriter.vue
@@ -2,6 +2,17 @@
     <div id="novelWriter">
       <h1>Tell me a story</h1>
 
+      <v-alert
+        id="storyAlert"
+        :value="storyError"
+        :type="storyErrorType"
+        dismissible
+        style="width: 80%"
+        @click="redirectToStoryCreation"
+      >{{storyErrorMessage}}</v-alert>
+
+      <v-select v-model="story" :items="userStories" label="Story" attach></v-select>
+
       <editor />
     </div>
 </template>
@@ -10,7 +21,46 @@
 import Editor from './Editor'
 export default {
   name: 'NovelWriter',
-  components: {Editor}
+  components: {Editor},
+  data () {
+    return {
+      story: '',
+      userStories: [],
+      storyError: false,
+      storyErrorType: 'warning',
+      storyErrorMessage: '',
+      serverDoesNotRespondErrorMessage: process.env.SERVER_DOES_NOT_RESPOND_ERROR_MESSAGE
+    }
+  },
+  mounted () {
+    this.$store.state.loader = true
+    this.$http.get(process.env.API_LOCATION + '/users/' + localStorage.userId + '/stories', {
+      headers: {
+        'Authorization': localStorage.accessToken
+      },
+      params: {
+        'filter': {'order': 'update_date DESC'}
+      }
+    }).then(response => {
+      let data = response.data
+
+      if (data.length === 0) {
+        this.storyError = true
+        this.storyErrorMessage = 'You don\'t have any story yet. Try to create one :)'
+        this.storyErrorType = 'warning'
+      }
+      this.userStories = response.data.map(story => story.title)
+    })
+      .catch(err => {
+        this.storyError = true
+        this.storyErrorMessage = this.serverDoesNotRespondErrorMessage
+        this.storyErrorType = 'error'
+        console.log(err)
+      })
+      .finally(() => {
+        this.$store.state.loader = false
+      })
+  }
 }
 </script>
 
