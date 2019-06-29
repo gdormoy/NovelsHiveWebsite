87a177198e93838e10050151467da8f859f04393
Vincent Guénin <guenin.vincent4@gmail.com>
2019-06-29 10:03:35 +0200

    [ADD] page & route /search

---
+++ src/components/SearchStory.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="storySearcher">
+    <h1>What do you want te read ?</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SearchStory'
+  }
+</script>
+
+<style scoped>
+
+</style>
