1232470d3ccca0fbee12e0f670bbfa16ab5fd02f
Vincent Guénin <guenin.vincent4@gmail.com>
2019-06-29 12:05:25 +0200

    [ADD] Display stories on title filter

--- src/components/SearchStory.vue
+++ src/components/SearchStory.vue
@@ -1,13 +1,77 @@
 <template>
   <div id="storySearcher">
-    <h1>What do you want te read ?</h1>
+    <h1>What do you want to read ?</h1>
+
+    <v-text-field
+      v-model="storyName"
+      placeholder="Story name"
+      prepend-inner-icon="search"
+      clearable
+      @input="storyNameChanged()"
+    ></v-text-field>
+
+    <v-list three-line>
+      <div
+        v-for="story in stories"
+        :key="story.id"
+      >
+        <h2>{{story.title}}</h2>
+        <p>{{story.synopsis}}</p>
+      </div>
+    </v-list>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'SearchStory'
+
+export default {
+  name: 'SearchStory',
+  data () {
+    return {
+      storyName: '',
+      stories: []
+    }
+  },
+  mounted () {
+    this.loadStories()
+  },
+  methods: {
+    storyNameChanged () {
+      this.loadStories()
+    },
+    loadStories () {
+      this.$http.get(process.env.API_LOCATION + '/stories', {
+        headers: {
+          'Authorization': localStorage.accessToken
+        },
+        params: {
+          'filter': {
+            'where': {'title': {'like': this.getStoryNameForSearch()}},
+            'limit': 20,
+            'skip': 0
+          }
+        }
+      }).then(response => {
+        console.log(response)
+        this.stories = response.data
+
+        this.stories.forEach((story) => {
+          story.synopsis = Buffer.from(story.synopsis).toString('utf-8')
+        })
+        console.log(this.stories)
+      })
+        .catch(error => console.log(error))
+    },
+    getStoryNameForSearch () {
+      let result = '%'
+      Array.from(this.storyName).forEach(char => {
+        result += char + '%'
+      })
+
+      return result
+    }
   }
+}
 </script>
 
 <style scoped>
