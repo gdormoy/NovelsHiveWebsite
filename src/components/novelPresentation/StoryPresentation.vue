03c0ad23c54133bc348960d0647be5ca0690b3c5
Vincent Guénin <guenin.vincent4@gmail.com>
2019-07-03 14:20:42 +0200

    [UPD] Almost beautiful display of chapters

--- src/components/novelPresentation/StoryPresentation.vue
+++ src/components/novelPresentation/StoryPresentation.vue
@@ -1,13 +1,13 @@
 <template>
-  <div id="story" style="text-align: left; text-justify: auto;">
-    <h1>{{story.title}}</h1>
-    <p><strong>Synopsis : </strong>{{story.synopsis}}</p>
+  <div id="story" style="margin-left: auto; margin-right: auto; width: 80%; text-justify: auto;">
+    <h1 style="text-decoration: underline">{{story.title}}</h1>
+    <p style="margin-bottom: 7%; margin-top: 3%;"><strong>Synopsis : </strong>{{story.synopsis}}</p>
 
-    <div v-for="chapter in story.storyChapters" :key="chapter.id">
-      <router-link :to="{ name: 'read', params: { id: chapter.id } }" v-if="chapter.online">
-        {{chapter.number}}/{{maxChapter}} : {{chapter.title}}
+    <div id="chapters" v-for="chapter in story.storyChapters" :key="chapter.id" style="margin-bottom: 1%">
+      <router-link :to="{ name: 'read', params: { id: chapter.id } }" v-if="chapter.online" tag="h3">
+        {{chapter.number}}. {{chapter.title}}
       </router-link>
-      <p v-else>{{chapter.number}}/{{maxChapter}} : {{chapter.title}}</p>
+      <h3 v-else><!--{{chapter.number}}/{{maxChapter}} : -->{{chapter.title}}</h3>
     </div>
   </div>
 </template>
@@ -40,5 +40,8 @@ export default {
 </script>
 
 <style scoped>
-
+  #chapters:hover {
+    cursor: pointer;
+    color: grey;
+  }
 </style>
