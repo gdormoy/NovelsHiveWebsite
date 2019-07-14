fe8a69cf4b3342e2b5604e8a144d2848b98638ba
Vincent Guénin <guenin.vincent4@gmail.com>
2019-07-14 16:45:15 +0200

    [ADD] Search filter on user finding

--- src/components/betaReaders/BetaReader.vue
+++ src/components/betaReaders/BetaReader.vue
@@ -3,7 +3,7 @@
       <h1>Beta Readers</h1>
       <div>
         <v-text-field
-          v-model="userName"
+          v-model="username"
           placeholder="Users"
           prepend-inner-icon="search"
           clearable
@@ -29,12 +29,6 @@
         >
         {{user.user.username}}
       </v-tab>
-      <v-btn fab dark color="indigo"
-        style="float: right"
-        @click="addBetaReaders"
-      >
-        <v-icon dark>add</v-icon>
-      </v-btn>
     </div>
 </template>
 
@@ -43,109 +37,71 @@ export default {
   name: 'BetaReader',
   data: () => ({
     storyId: '',
-    userName: '',
-    usersTab: [],
-    usersId: [],
-    users: [],
+    username: '',
     userList: [],
+    searchMatchingUsers: [],
     betaReaders: []
   }),
   created () {
     this.storyId = this.$route.params.id
 
-    let promises = []
-
-    promises.push(this.$http.get(process.env.API_LOCATION + '/stories/' + this.storyId + '/betaReaders', {
-      headers: {
-        'Authorization': localStorage.accessToken
-      },
-      params: {
-        'filter': {
-          'include': 'user'
+    let promises = [
+      this.$http.get(process.env.API_LOCATION + '/stories/' + this.storyId + '/betaReaders', {
+        params: {
+          'filter': {
+            'include': 'user'
+          }
         }
-      }
-    }))
-
-    promises.push(this.$http.get(process.env.API_LOCATION + '/users', {
-      headers: {
-        'Authorization': localStorage.accessToken
-      }
-    }))
+      }),
+      this.getUsers()
+    ]
 
     Promise.all(promises).then(values => {
-      console.log(values)
       this.betaReaders = values[0].data
+      this.getBetaReadersUsersCandidates(values[1].data)
+    })
+  },
+  methods: {
+    getUsers () {
+      return this.$http.get(process.env.API_LOCATION + '/users', {
+        headers: {
+          'Authorization': localStorage.accessToken
+        },
+        params: {
+          filter: {
+            where: {
+              username: {
+                like: this.getUserNameForSearch()
+              }
+            }
+          }
+        }
+      })
+    },
+    getBetaReadersUsersCandidates (allUsers) {
       let betaReaders = this.betaReaders.map(betaReader => betaReader.user)
 
-      this.userList = values[1].data.filter(user => {
+      this.userList = allUsers.filter(user => {
         if (user.id === parseInt(localStorage.userId)) return false
 
         for (let i = 0; i < betaReaders.length; i++) {
           if (betaReaders[i].id === user.id) {
-            console.log('exists')
             return false
           }
         }
         return true
       })
-    })
-  },
-  methods: {
-    searchParamsChanged () {
-      this.loadUsers()
     },
-    loadUsers () {
-      let userFilter = {
-        'username': {
-          'like': this.getUserNameForSearch()
-        }
-      }
-
-      let whereArray = [userFilter]
-
-      let requestParam = {
-        headers: {
-          'Authorization': localStorage.accessToken
-        }
-      }
-
-      let filterParam = {
-        params: {
-          'filter': {
-            'where': {'and': whereArray}
-          }
-        }
-      }
-
-      Object.assign(requestParam, filterParam)
-
-      this.$http.get(process.env.API_LOCATION + '/users', requestParam)
-        .then(res => {
-          let usersTab = res.data
-          let usersId = this.usersId
-          let indexUser = ''
-          let indexTab = []
-          usersTab.forEach(function (user) {
-            if (user.id === parseInt(localStorage.userId)) {
-              indexUser = usersTab.indexOf(user)
-              usersTab.splice(indexUser, 1)
-            } else {
-              let exist = usersId.includes(user.id)
-              if (exist) {
-                indexTab.push(usersTab.indexOf(user))
-              }
-            }
-          })
-          indexTab.sort((a, b) => b - a)
-          indexTab.forEach(function (index) {
-            usersTab.splice(index, 1)
-          })
-          this.usersTab = usersTab
-        })
+    searchParamsChanged () {
+      console.log('searchParamsChanged')
+      this.getUsers().then(response => {
+        console.log(response)
+        this.getBetaReadersUsersCandidates(response.data)
+      })
     },
     getUserNameForSearch () {
       let result = '%'
-      Array.from(this.userName).forEach(char => {
+      Array.from(this.username).forEach(char => {
         result += char
       })
       result += '%'
@@ -168,36 +124,6 @@ export default {
       this.$http.delete(process.env.API_LOCATION + /beta_readers/ + betaReader.id)
         .then(response => console.log(response))
         .catch(error => console.log(error))
-
-      // let index = this.users.indexOf(user)
-      // let idIndex = this.usersId.indexOf(user.userId)
-      // this.usersId.splice(idIndex, 1)
-      // this.users.splice(index, 1)
-      // if (user.id >= 0) {
-      //   this.$http.delete(process.env.API_LOCATION + '/beta_readers/' + user.id, {
-      //     headers: {
-      //       'Authorization': localStorage.accessToken
-      //     }
-      //   })
-      // }
-    },
-    addBetaReaders () {
-      let storyId = this.storyId
-      let request = this.$http
-      this.users.forEach(function (user) {
-        if (user.id < 0) {
-          request.post(process.env.API_LOCATION + '/beta_readers/', {
-            header: {
-              'Authorization': localStorage.accessToken
-            },
-            'userId': user.userId,
-            'storyId': storyId
-          })
-            .then(res => {
-              user.id = res.data.id
-            })
-        }
-      })
     }
   }
 }
