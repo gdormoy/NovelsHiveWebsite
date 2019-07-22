7a9a9034c81d964295b048a1c46cc9a2e706c69b
Vincent Guénin <guenin.vincent4@gmail.com>
2019-07-22 21:42:02 +0200

    [ADD] API url to env variable

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,6 @@
 
 module.exports = {
   NODE_ENV: '"production"',
-  API_LOCATION: '"http://localhost:3000/api"',
+  API_LOCATION: '"http://ecs-first-run-alb-113054812.eu-west-3.elb.amazonaws.com:3000//api"',
   SERVER_DOES_NOT_RESPOND_ERROR_MESSAGE: '\'Whooops the server is broken. We are fixing it, try again later :)\''
 };
